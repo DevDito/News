64c0887a7f325e9b2005efc2541784af8de34ee8
Satria Amru Dito <developerdito@email.com>
2021-10-04 08:45:50 +0700

    Initial Commit

---
+++ News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  News
+//
+//  Created by Satria Amru Dito on R 3/10/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
